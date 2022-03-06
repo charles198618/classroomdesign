d18fa105871d4c58528b716e3663f92947df23c2
Alliance <allytshindayi@gmail.com>
2022-03-06 22:39:28 +0100

    the header

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,28 @@
 module.exports = {
-  content: {
-    content: ['./public/**/*html']
-  },
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
+	purge: [],
+	darkMode: 'class', // or 'media' or 'class'
+	theme: {
+		extend: {
+			colors: {
+				primary: {
+					default: '#613bdb',
+				},
+			},
+			fontFamily: {
+				title: ['Mote'],
+			},
+			screens: {
+				'3xl': '1600px',
+			},
+		},
+		container: {
+			center: true,
+		},
+	},
+	variants: {
+		extend: {
+			textOpacity: ['dark'],
+		},
+	},
+	plugins: [],
+};
